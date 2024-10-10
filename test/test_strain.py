85c2e144188de84d97ba5b27ceab40efc384da23
Diego <diego.degusem@ugent.be>
2024-10-10 12:33:59 +0200

    Make test clearer

--- test/test_strain.py
+++ test/test_strain.py
@@ -23,12 +23,12 @@ def max_semirelative_error(result, wanted):
     return max_absolute_error(result, wanted) / np.max(abs(wanted))
 
 
-def create_magnet(d_comp, cos_comp):
+def create_magnet(i_comp, j_comp):
     """Makes a world with a 1D magnet in the d_comp direction.
     """
     gridsize, gridsize_magnet, pbc_repetitions = [0, 0, 0], [1, 1, 1], [0, 0, 0]
-    gridsize[d_comp], pbc_repetitions[d_comp] = N, 1  # set for PBC grid
-    gridsize_magnet[d_comp] = N
+    gridsize[i_comp], pbc_repetitions[i_comp] = N, 1  # set for PBC grid
+    gridsize_magnet[i_comp] = N
 
     world = World(cellsize, mastergrid=Grid(gridsize), pbc_repetitions=pbc_repetitions)
     
@@ -37,45 +37,54 @@ def create_magnet(d_comp, cos_comp):
 
     magnet.enable_elastodynamics = True  # just in case
 
-    L = N*cellsize[d_comp]
+    L = N*cellsize[i_comp]
     k = P*2*math.pi/L
 
     def displacement_func(x, y, z):
         u = [0,0,0]
-        comp = (x,y,z)[d_comp]
-        u[d_comp] = A * math.sin(k * comp)
-        u[cos_comp] = A * math.cos(k * comp)
+        comp = (x,y,z)[i_comp]
+        u[j_comp] = A * math.sin(k * comp)
         return tuple(u)
 
     magnet.elastic_displacement = displacement_func
     strain_num = magnet.strain_tensor.eval()
     
     strain_anal = np.zeros(shape=strain_num.shape)
-    strain_anal[d_comp,...] = k * magnet.elastic_displacement.eval()[cos_comp,...]
-    strain_anal[2 + d_comp + cos_comp,...] = - 0.5 * k * magnet.elastic_displacement.eval()[d_comp,...]
+
+    # derivative of sine is cosine
+    cos = np.zeros(shape=strain_anal[0,...].shape)
+    index = [slice(None), slice(None), slice(None)]
+    index[(i_comp+1)%3] = 0  # indexes with length 1
+    index[(i_comp+2)%3] = 0  # indexes with length 1
+    cos[index[2], index[1], index[0]] = A * np.cos(k * cellsize[i_comp] * np.arange(0,N))
+    if i_comp == j_comp:
+        strain_anal[j_comp,...] = k * cos
+    else:
+        strain_anal[2 + i_comp + j_comp,...] = 0.5 * k * cos
+    
     assert max_semirelative_error(strain_num, strain_anal) < RTOL
 
 
-def test_sinx_cosx_0():
-    d_comp = 0
-    create_magnet(d_comp, (d_comp+1)%3)
+def test_Exx():
+    i, j = 0, 0
+    create_magnet(i, j)
 
-def test_0_siny_cosy():
-    d_comp = 1
-    create_magnet(d_comp, (d_comp+1)%3)
+def test_Eyy():
+    i, j = 1, 1
+    create_magnet(i, j)
 
-def test_cosz_0_sinz():
-    d_comp = 2
-    create_magnet(d_comp, (d_comp+1)%3)
+def test_Ezz():
+    i, j = 2, 2
+    create_magnet(i, j)
 
-def test_sinx_0_cosx():
-    d_comp = 0
-    create_magnet(d_comp, (d_comp+2)%3)
+def test_Exy():
+    i, j = 0, 1
+    create_magnet(i, j)
 
-def test_cosy_siny_0():
-    d_comp = 1
-    create_magnet(d_comp, (d_comp+2)%3)
+def test_Exz():
+    i, j = 0, 2
+    create_magnet(i, j)
 
-def test_0_cosz_sinz():
-    d_comp = 2
-    create_magnet(d_comp, (d_comp+2)%3)
+def test_Eyz():
+    i, j = 1, 2
+    create_magnet(i, j)
