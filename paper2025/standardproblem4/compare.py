09740bef5a25b6c853256a500da602816c7cd782
Jonathan Maes <jonathan145b@hotmail.com>
2025-10-15 22:02:02 +0200

    Add script for standardproblem4 mumax+/3/OOMMF comparison

---
+++ paper2025/standardproblem4/compare.py
@@ -0,0 +1,191 @@
+"""
+This script generates the data from Fig. 3 of the paper
+'mumax+: extensible GPU-accelerated micromagnetics and beyond' (https://arxiv.org/abs/2411.18194)
+and uses the mumax+ version v1.1.1.
+
+This simulation can take a significant time to complete (depending on your machine).
+Note that an installation of mumax3 is necessary in order to generate this comparison.
+"""
+
+
+import argparse
+import matplotlib.pyplot as plt
+import mumaxplus
+import numpy as np
+import os
+import pandas as pd
+import re
+import requests
+import subprocess
+
+from matplotlib.widgets import MultiCursor
+from pathlib import Path
+from typing import Literal
+
+import matplotlib
+matplotlib.rc('font', family='serif', size=7)
+plt.rc('text', usetex=True)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("mpdir", nargs='?', default=r"..\..", help="The mumax+ source directory, needed to recompile between 32- and 64-bit.")
+args = parser.parse_args()
+
+MUMAXPLUS_SOURCEDIR: str = args.mpdir
+MUMAX3_COMMAND = r"mumax3" # Change to executable path if mumax3 is not in $env:PATH
+DATA_DIR = "data"
+OUTPUT_DIR = "figures"
+
+
+def set_mumaxplus_precision(precision: Literal["SINGLE", "DOUBLE"]):
+    if precision not in ["SINGLE", "DOUBLE"]: raise ValueError("Precision must be SINGLE or DOUBLE.")
+    if precision != mumaxplus.FP_PRECISION:
+        env = os.environ.copy()
+        env["MUMAXPLUS_FP_PRECISION"] = precision
+        subprocess.check_call(["pip", "install", "."], cwd=MUMAXPLUS_SOURCEDIR, env=env)
+
+def run_all(results_dir: str|Path = DATA_DIR):
+    results_dir = Path(results_dir).absolute()
+    results_dir.mkdir(parents=True, exist_ok=True)
+    print(f"Saving results to {results_dir} ...")
+    
+    ## OOMMF
+    url = "https://www.ctcms.nist.gov/~rdm/std4/Donahue/stdprob4a.odt" # 1nm cell size, OOMMF eXtensible solver
+    r = requests.get(url)
+    with open(results_dir / "standardproblem4a.odt", 'wb') as outfile:
+        outfile.write(r.content)
+    
+    ## mumax+ and mumax3
+    set_mumaxplus_precision("SINGLE")
+    for cs in ["2.5nm", "1nm"]: # Do the fastest first
+        results_dir_here = results_dir / cs
+        subprocess.run(f'python standardproblem4a_{cs}.py "{results_dir_here}"') # mumax+ 32 bit only
+        subprocess.run(f'{MUMAX3_COMMAND} -o="{results_dir_here / "standardproblem4a.out"}" standardproblem4a_{cs}.mx3')
+
+def read_ODT(file: str|Path):
+    """ Parses the submitted time series at https://www.ctcms.nist.gov/~rdm/std4/Donahue.html """
+    file = Path(file)
+    lines = []
+    with open(file, "r") as inFile:
+        for line in inFile:
+            parts = line.strip().split(" ")
+            parts = [part for part in parts if part]
+            lines.append(parts)
+    
+    columns_str = " ".join(lines[3]).replace("# Columns: ", "").strip()
+    columns: list[str] = re.findall(r'\{[^}]+\}|\S+', columns_str)
+    columns = [col.strip(r"{}") for col in columns] # .split(":")[-1]
+    
+    units_str = " ".join(lines[4]).replace("# Units: ", "").strip()
+    units: list[str] = re.findall(r'\{[^}]+\}|\S+', units_str)
+    units = [unit.strip(r"{}") for unit in units]
+    
+    data = lines[5:] # First 3 lines are irrelevant for us
+    
+    df = pd.DataFrame(data, columns=columns)
+    df = df.apply(pd.to_numeric, errors='coerce')
+    useful_cols = {"Oxs_TimeDriver::Simulation time": "time",
+                   "Oxs_TimeDriver::mx": "mx",
+                   "Oxs_TimeDriver::my": "my",
+                   "Oxs_TimeDriver::mz": "mz",
+                   "Oxs_RungeKuttaEvolve:evolver:Total energy": "e_total",
+                   "Oxs_UniformExchange::Energy": "e_exchange",
+                   "Oxs_FixedZeeman::Energy": "e_zeeman",
+                   "Oxs_Demag::Energy": "e_demag"
+    }
+    
+    df_useful = df[[key for key in useful_cols.keys() if key in df.columns]]
+    return df_useful.rename(columns=useful_cols)
+
+
+def main(ERRORLEVEL=0):
+    ## LOAD DATA
+    try:
+        dfs = {
+            "OOMMF_1nm": read_ODT(DATA_DIR + "/standardproblem4a.odt"),
+            "mumax+32_2.5nm": pd.read_csv(DATA_DIR + "/2.5nm/standardproblem4a_plus_SINGLE.out", sep="\t"),
+            "mumax3_1nm": pd.read_csv(DATA_DIR + "/1nm/standardproblem4a.out/table.txt", sep="\t"),
+            "mumax3_2.5nm": pd.read_csv(DATA_DIR + "/2.5nm/standardproblem4a.out/table.txt", sep="\t"),
+            "mumax+32_1nm": pd.read_csv(DATA_DIR + "/1nm/standardproblem4a_plus_SINGLE.out", sep="\t")
+        }
+    except Exception as e:
+        if ERRORLEVEL: raise e
+        run_all()
+        return main(ERRORLEVEL=ERRORLEVEL+1)
+    
+    for key, df in dfs.items():
+        if key.startswith("mumax3"):
+            df.rename(columns={"# t (s)": "time", "mx ()": "mx", "my ()": "my", "mz ()": "mz"}, inplace=True)
+        elif key.startswith("mumax+"):
+            df.rename(columns={"# time": "time"}, inplace=True)
+    
+    ## PLOTTING
+    plot_comps = ["y"]
+    fig, axes = plt.subplots(nrows=1, ncols=len(plot_comps), figsize=(2.5*len(plot_comps), 4.8/6.4 * 2.5), sharex=True, sharey=True)
+    axes = np.atleast_2d(axes)
+    t_factor, t_unit = 1e9, "n"
+    # m_factor, m_unit = 1e3, r"\textperthousand"
+    # m_factor, m_unit = 1e2, r"\%"
+    m_factor, m_unit = 1e3, r"10^{-3}"
+    t_max = 3e-9
+    
+    tools_names = {"OOMMF": "OOMMF", "mumax3": "mumax$^3$", "mumax+32": r"mumax$^{\!+}$"}
+    tools_colors = {"OOMMF": "C6", "mumax3": "C0", "mumax+32": "C1"}
+    cs_names = {"1nm": r"$1\!\times\!1\!\times\!1$ nm$^3$", "2.5nm": r"$2.5\!\times\!2.5\!\times\!3$ nm$^3$"}
+    cs_linestyles = {"1nm": "-", "2.5nm": "--"}
+    lw = .7
+    
+    base = "OOMMF_1nm"
+    df_base = dfs[base]
+    t = df_base["time"].to_numpy()
+    deviation = lambda df, col: np.interp(x=t, xp=df["time"], fp=df[col]) - df_base[col].to_numpy()
+    
+    # Difference with baseline, for each magnetisation component
+    for i, comp in enumerate(plot_comps):
+        ax: plt.Axes = axes[0,i]
+        for key, df in dfs.items():
+            if key == base:
+                ax.plot(t*t_factor, np.zeros_like(t), color="grey", lw=1, ls="-")
+                continue
+            tool, cs = key.split("_")
+            color = tools_colors[tool]
+            ls = cs_linestyles[cs]
+            ax.plot(t*t_factor, deviation(df, f"m{comp}")*m_factor, color=color, ls=ls, lw=lw)
+        
+    # Legend entries
+    for tool, color in tools_colors.items():
+        if tool == base.split("_")[0]: continue
+        ax.fill_between([], [], [], color=color, label=f"{tools_names[tool]}")
+    for cs, ls in cs_linestyles.items():
+        ax.plot([], [], color="k", ls=ls, lw=lw, label=f"{cs_names[cs]}")
+    
+    if len(plot_comps) == 1:
+        ax.legend(ncols=2, bbox_to_anchor=(0, 1.12, 1, 0.2), handletextpad=0.7, handlelength=2, loc="upper left", mode="expand", borderaxespad=0)
+    else:
+        fig.legend(*ax.get_legend_handles_labels(), ncols=2, loc="upper center")
+    t_max = min(df["time"].to_numpy().max() for df in dfs.values())
+    ax.set_xlim([0, t_max*t_factor])
+    # ax.ticklabel_format(axis="y", style="scientific", scilimits=[-1,1])
+    ax.set_xlabel(f"Time ({t_unit}s)")
+    ax.set_ylabel(f"$\\Delta m_{comp}$" + f" $/\\,{m_unit}$"*(m_unit != ""))
+    in_range = np.where(np.logical_and(t >= 0, t <= t_max))
+    ymax = max([np.max(np.abs(deviation(df, f"m{comp}")[in_range])) for df in dfs.values() for comp in plot_comps])*m_factor
+    pady = 0.05
+    ax.set_ylim([-ymax*(1+pady), ymax*(1+pady)])
+    
+    multi = MultiCursor(None, tuple(axes.flat), horizOn=False, vertOn=True, color='k', lw=1, ls=":")
+    
+    # title = f"Standard problem 4 comparison with {tools_names[base.split('_')[0]]}"
+    # if len(plot_comps) == 1: ax.set_title(title)
+    # else: fig.suptitle(title)
+    fig.tight_layout(h_pad=0)
+    fig.subplots_adjust(top=0.78, bottom=0.18)
+    for ext in ("pdf", "png", "svg"):
+        outfile = Path(f"{OUTPUT_DIR}/comparison_{base}.{ext}").absolute()
+        outfile.parent.mkdir(exist_ok=True)
+        fig.savefig(outfile, dpi=1200)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ paper2025/standardproblem4/standardproblem4a_1nm.py
@@ -0,0 +1,46 @@
+#!/bin/env python3
+
+# This script solves micromagnetic standard problem 4. The Problem specification
+# can be found on https://www.ctcms.nist.gov/~rdm/mumag.org.html
+
+import argparse
+import numpy as np
+
+from mumaxplus import Ferromagnet, Grid, World, FP_PRECISION
+
+parser = argparse.ArgumentParser()
+parser.add_argument("outdir", nargs='?', default=r"./results", help="The output file.")
+args = parser.parse_args()
+
+length, width, thickness = 500e-9, 125e-9, 3e-9
+nx, ny, nz = 500, 125, 3
+world = World(cellsize=(length / nx, width / ny, thickness / nz))
+
+magnet = Ferromagnet(world, Grid((nx, ny, nz)))
+magnet.msat = 800e3
+magnet.aex = 13e-12
+magnet.alpha = 0.02
+
+magnet.magnetization = (1, 0.1, 0)
+magnet.relax()
+
+B1 = (-24.6e-3, 4.3e-3, 0)
+B2 = (-35.5e-3, -6.3e-3, 0)
+world.bias_magnetic_field = B1  # choose B1 or B2 here
+
+# --- SCHEDULE THE OUTPUT ---
+
+timepoints = np.linspace(0, 3e-9, 3001)
+outputquantities = {
+    "mx": lambda: magnet.magnetization.average()[0],
+    "my": lambda: magnet.magnetization.average()[1],
+    "mz": lambda: magnet.magnetization.average()[2],
+    "e_total": magnet.total_energy,
+    "e_exchange": magnet.exchange_energy,
+    "e_zeeman": magnet.zeeman_energy,
+    "e_demag": magnet.demag_energy
+}
+
+# --- RUN THE SOLVER ---
+
+output = world.timesolver.solve(timepoints, outputquantities, args.outdir + f"/standardproblem4_plus_{FP_PRECISION}.out")
