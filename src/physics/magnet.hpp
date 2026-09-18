3a6d69ca4a8b61a40d87ec88d77facb986fa76ad
DiegoDeGusem <diego.degusem@ugent.be>
2026-09-18 10:05:34 +0200

    use clang-format file

--- src/physics/magnet.hpp
+++ src/physics/magnet.hpp
@@ -59,9 +59,8 @@ class Magnet {
   void removeStrayField(const Magnet*);
 
  private:
-  std::shared_ptr<System>
-      system_;  // the system_ has to be initialized first,
-                // hence its listed as the first datamember here
+  std::shared_ptr<System> system_;  // the system_ has to be initialized first,
+                                    // hence its listed as the first datamember here
   std::string name_;
   std::map<const Magnet*, StrayField*> strayFields_;
 
@@ -84,9 +83,7 @@ class Magnet {
   VectorParameter rigidNormStrain;
   VectorParameter rigidShearStrain;
 
-  // clang-format off
   BoundaryTraction boundaryTraction;  // Externally applied traction at the boundaries
-  // clang-format on
 
   // stiffness constants; TODO: can this be generalized to a 6x6 tensor?
   Parameter C11;  // C11 = C22 = C33
